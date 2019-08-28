ce31d55c002b992fd6522eb1d41f270f6b4addf9
unknown <21090563+derek-shnosh@users.noreply.github.com>
2019-08-28 14:15:35 -0700

    add ssh test, update logging, organize code

--- ztp-watcher.py
+++ ztp-watcher.py
@@ -7,23 +7,11 @@ import os
 import threading
 import logging
 import yaml
+import socket
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command, netmiko_send_config
-
-
-with open('./ztpconfig.yaml', 'r') as f:
-    config = yaml.safe_load(f)
-
-logfile = config['logfile']
-watch_dir = config['watch_dir']
-tftpaddr = config['tftpaddr']
-imgfile = config['imgfile']
-username = config['username']
-password = config['password']
-
-ignorefiles = ['.swp', '.save']
 
 
 def std_log(agg_result):
@@ -51,7 +39,7 @@ class Watcher:
         event_handler = Handler()
         self.observer.schedule(event_handler, watch_dir, recursive=False)
         self.observer.start()
-        Logger('Starting FreeZTP Provisioning Watcher.')
+        Logger('Starting ZTP Provisioning Watcher.')
         try:
             while True:
                 time.sleep(5)
@@ -59,7 +47,7 @@ class Watcher:
         except KeyboardInterrupt:
             self.observer.stop()
             print('\nKeyboard interrupt.')
-            Logger('Stopping FreeZTP Provisioning Watcher (Keyboard interrupt).')
+            Logger('Stopping ZTP Provisioning Watcher (Keyboard interrupt).')
 
         except:
             self.observer.stop()
@@ -69,10 +57,57 @@ class Watcher:
 
 class Handler(FileSystemEventHandler):
 
-    def os_upgrade(self, hostname, hostaddr, tftpaddr, imgfile):
+    def on_created(self, event):
 
-        time.sleep(45)
-        Logger(f'{hostname} starting TFTP image transfer.')
+        ignorefiles = ['.swp', '.save']
+
+        if event.is_directory:
+            return None
+        else:
+            newfile = event.src_path.rpartition('/')[2]
+            if not any(str in newfile for str in ignorefiles):
+                Logger(f'File created: {newfile}')
+                hostname = newfile.split('_')[0]
+                hostaddr = newfile.split('_')[1]
+                x = threading.Thread(target=self.test_ssh, args=(
+                    hostname, hostaddr))
+                x.start()
+
+    def test_ssh(self, hostname, hostaddr, port=22):
+
+        initialwait = 15
+        retrywait = 3
+        attempts = 0
+        maxattempts = 25
+
+        Logger(f'{hostname}: Attempting SSH to {hostaddr} in {initialwait}s.')
+        time.sleep(initialwait)
+
+        result = None
+        while result is None:
+            try:
+                attempts += 1
+                testconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                testconn.connect((hostaddr, port))
+            except Exception as e:
+                if attempts >= maxattempts:
+                    Logger(
+                        f'{hostname}: SSH attempts exhausted ({maxattempts}); {e}.')
+                    break
+                else:
+                    time.sleep(retrywait)
+                    continue
+            else:
+                result = testconn
+                testconn.close()
+                Logger(
+                    f'{hostname}: SSH connection success after {attempts} attempt(s) -> copy image file.')
+                self.os_upgrade(hostname, hostaddr)
+                break
+
+    def os_upgrade(self, hostname, hostaddr):
+
+        Logger(f'{hostname}: Starting TFTP image transfer.')
 
         nr = InitNornir(
             inventory={
@@ -88,13 +123,16 @@ class Handler(FileSystemEventHandler):
                 }
             }
         )
+        copystart = time.time()
         copyfile = nr.run(
             task=netmiko_send_command,
             command_string=f'copy tftp://{tftpaddr}/{imgfile} flash:',
             delay_factor=6,
         )
-        std_log(copyfile)
-        Logger(f'{hostname} TFTP transfer completed, setting boot variable.')
+        copyduration = round(time.time() - copystart)
+        # std_log(copyfile)
+        Logger(
+            f'{hostname}: Image transfer completed after {copyduration}s -> set boot variable.')
 
         bootcmds = f'default boot sys\nboot system flash:{imgfile}'
         bootcmds_list = bootcmds.splitlines()
@@ -102,34 +140,30 @@ class Handler(FileSystemEventHandler):
             task=netmiko_send_config,
             config_commands=bootcmds_list
         )
-        std_log(bootvar)
-        Logger(f'{hostname} boot variable has been set, writing config.')
+        # std_log(bootvar)
+        Logger(f'{hostname}: Boot variable set -> write config.')
 
         writemem = nr.run(
             task=netmiko_send_command,
             command_string='write mem',
         )
-        std_log(writemem)
-        Logger(f'{hostname} config written, ready to reload/power off.')
+        # std_log(writemem)
+        Logger(f'{hostname}: Config written, ready to reload/power off.')
 
-    def on_created(self, event):
-
-        if event.is_directory:
-            return None
-
-        else:
-            newfile = event.src_path.rpartition('/')[2]
-            if not any(str in newfile for str in ignorefiles):
-                Logger(f'File created: {newfile}')
-                hostname = newfile.split('_')[0]
-                hostaddr = newfile.split('_')[1]
-                Logger(
-                    f'Waiting 45 seconds for TFTP image transfer to {hostname} (IP: {hostaddr}).')
-                x = threading.Thread(target=self.os_upgrade, args=(
-                    hostname, hostaddr, tftpaddr, imgfile))
-                x.start()
+        nr.close_connections
 
 
 if __name__ == '__main__':
+
+    with open('./ztpconfig.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    logfile = config['logfile']
+    watch_dir = config['watch_dir']
+    tftpaddr = config['tftpaddr']
+    imgfile = config['imgfile']
+    username = config['username']
+    password = config['password']
+
     w = Watcher()
     w.run()
